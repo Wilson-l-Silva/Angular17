39cc0de3e0ffd2c56d8f807da05deb03b9b63bf1
Wilson <wilson.lima.silva@icloud.com>
2025-05-23 01:01:25 -0300

    Adicionado modulo Cards e seus componentes, adicionado interpolação usando interfaces para tipar objeto

--- TESTES/projeto-components/src/app/card/card.component.ts
+++ TESTES/projeto-components/src/app/card/card.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
 
+
+interface Iplano{
+  infos: Iinfos;
+}
+
+interface Iinfos{
+  tipo: string,
+  preco: number,
+}
+
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.scss']
 })
-export class CardComponent {
 
+export class CardComponent {
+  //ignora qualquer verificação de tipagem
+  //@ts-ignore
+  plano: Iplano = {
+    infos: {
+      tipo: 'Simples',
+      preco: 100,
+    },
+  }
 }
